8258b04926390732544701cd2b26ef8ee32f70dd
hsnsh-aras <hasansahin@araskargo.com.tr>
2025-06-22 00:52:42 +0300

    ok

--- src/Eds.Shared.Contracts/Events/GibInvoice/SendGibSysQueueResultEto.cs
+++ src/Eds.Shared.Contracts/Events/GibInvoice/SendGibSysQueueResultEto.cs
@@ -3,13 +3,19 @@ using JetBrains.Annotations;
 
 namespace Eds.Shared.Contracts.Events.GibInvoice;
 
-public sealed record IntegrationSalesInvoiceAnswerEnvelopeResultEto(
+public sealed record SendGibSysQueueResultEto(
     Guid ReceivedGibDocQueueId,
+    Guid SendGibSysQueueId,
+    [NotNull] string SendGibSysQueueResponseCode,
+    [NotNull] string SendGibSysQueueResponseDescription,
     bool IsOperationSuccess,
     [CanBeNull] string OperationDescription
 ) : IIntegrationEventMessage
 {
     public Guid ReceivedGibDocQueueId { get; } = ReceivedGibDocQueueId;
+    public Guid SendGibSysQueueId { get; } = SendGibSysQueueId;
+    [NotNull]  public string SendGibSysQueueResponseCode { get; } = SendGibSysQueueResponseCode;
+    [NotNull]  public string SendGibSysQueueResponseDescription { get; } = SendGibSysQueueResponseDescription;
     public bool IsOperationSuccess { get; } = IsOperationSuccess;
     [CanBeNull] public string OperationDescription { get; } = OperationDescription;
 }
